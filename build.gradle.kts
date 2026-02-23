5a33d889647db90ad7b432d849b954f5ab8874cf
Bryce Peters <brycepeters04@gmail.com>
2026-02-23 16:30:38 -0700

    append version name from manifest to jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,14 @@
  * NOTE: This is entirely optional and basics can be done in `settings.gradle.kts`
  */
 
+import groovy.json.JsonSlurper
+
+val manifestVersion: String by lazy {
+    val manifestFile = file("src/main/resources/manifest.json")
+    val json = JsonSlurper().parseText(manifestFile.readText()) as Map<*, *>
+    json["Version"]?.toString() ?: error("manifest.json missing Version")
+}
+
 repositories {
     // Any external repositories besides: MavenLocal, MavenCentral, HytaleMaven, and CurseMaven
 }
@@ -10,4 +18,8 @@ dependencies {
     compileOnly(libs.jetbrains.annotations)
     compileOnly(libs.jspecify)
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+}
+
+tasks.withType<Jar> {
+    archiveFileName.set("MobFarmingTools-$manifestVersion.jar")
 }
